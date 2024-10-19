d9ab3cdb04240b573e938c07627abc769b367297
Joaorodrigues1706 <163606174+Joaorodrigues1706@users.noreply.github.com>
2024-10-18 22:03:39 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,7 @@
+const nomes = ["ALISSON", "NICOLE", "ALICE", "GUSTAVO", "PAOLA", "NEYMAR", "ANA"]
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
